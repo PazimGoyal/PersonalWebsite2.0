a3e36219660b661284eb7491ff4e5792337d459c
pazimgoyal <pazimgoyal@gmail.com>
2020-12-23 12:52:39 +0530

    work_experience complete

--- css/js/main.js
+++ css/js/main.js
@@ -74,7 +74,6 @@ $(function () {
       opacity:'0'
     },1000,function(){
       $(this).hide();
-
       $("#workexp").show().animate({
         opacity:'1'
       },1000,function(){
@@ -84,6 +83,33 @@ $(function () {
       });
     });
   })
+
+
+  $(".grid2 .img").mouseenter(function () {
+      // $("#name").text($(this).data("name")).show(500);
+      // $(".flex-center2").css("justify-content","start");
+      let text_exp=" "+$(this).data('info');
+      let new_text='';
+      $('.work_info').html(text_exp).css('opacity',0.1);      
+      for(let i=0;i<5000;i++){
+        new_text += text_exp;
+      }
+      $('.grid2_item.e1').html('Starting Date :<br>'+$(this).data('dates_start'));
+      $('.grid2_item.e2').html('Ending Date :<br>'+$(this).data('dates_end'));
+      $('.grid2_item.e5').html('Jobs Position :<br>' + $(this).data('position'));
+      $('.grid2_item.e6').html('Location :<br>' + $(this).data('location'));
+      $('.grid2_item.e7').html('References: <br>'+$(this).data('refrence'));
+
+      $('.work_info').append(new_text);
+      $(".hide").css('opacity',0);
+      $('.grid2_item.extra').css('opacity',1);
+     $(this).css('opacity',1);
+    })    .mouseleave(function () {
+     $(".work_info").css('opacity',0);      
+     $(".hide").css('opacity',1);
+     $('.grid2_item.extra').css('opacity',0);
+
+    });
 
 
 });
